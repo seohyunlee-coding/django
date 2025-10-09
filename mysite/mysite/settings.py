c5db97cf79a05053695d35744f3a2ad6745c7d34
Seohyun Lee <cwijiq3085@gmail.com>
2025-10-09 19:30:36 +0900

    html등 추가

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media/" 
+
+REST_FRAMEWORK = {
+'DEFAULT_PERMISSION_CLASSES': [
+# 'rest_framework.permissions.IsAdminUser’,
+],
+'PAGE_SIZE': 10
+}
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # include blog app urls so the root URL can render the blog post list
     path('', include('blog.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
