ac4e0ea6efc49f2d8fb4697513f5cc9a27ded145
Seohyun Lee <cwijiq3085@gmail.com>
2025-10-09 21:15:30 +0900

    html수정

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token #added
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # include blog app urls so the root URL can render the blog post list
     path('', include('blog.urls')),
+    path('api-token-auth/', obtain_auth_token), #added
 ]
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
